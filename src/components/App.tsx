3ccd1fa26249f8c13894affccf5fd3d60195c3d7
yen <l.yennie.j@gmail.com>
2022-11-29 03:54:17 +0900

    react-router-dom v5

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import firebase from "firebase/compat/app";
+import AppRouter from "./AppRouter";
 function App() {
   console.log(firebase);
-  return <div></div>;
+  return (
+    <div>
+      <AppRouter />
+    </div>
+  );
 }
 
 export default App;
